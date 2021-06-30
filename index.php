19fd4e0faa11e713dcc8903ab4b4e03097d27548
M.O Akindayo <matthewakinse@gmail.com>
2021-06-30 07:19:08 +0300

    App project starter

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
-<?php
-echo "Hello, user"
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Student Management</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+  <script   src="https://code.jquery.com/jquery-3.6.0.min.js"   
+    integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="   crossorigin="anonymous"></script>
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.25/datatables.min.css"/>
+  <script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.10.25/datatables.min.js"></script>
+</head>
+<body>
+  <h1>Student Management</h1>
+  <table id="course_table" class="table table-striped">
+    <thead bgcolor="#6cd8fe">
+      <tr class="table-primary">
+          <th width="30%">ID</th>
+          <th width="50%">Course Name</th>
+          <th width="30%">Number of Students</th>
+          <th scope="col" width="5%">Edit</th>
+          <th scope="col" width="5%">Delete</th>
+      </tr>
+    </thead>
+  </table>
+</body>
+</html>
